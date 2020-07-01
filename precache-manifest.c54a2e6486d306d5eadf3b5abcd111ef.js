07f241241ba0912df41ab4cad277fdbdcb77dec9
sidhant <sidhantsharma293@gmail.com>
2020-07-01 17:04:36 +0530

    Updates

---
+++ precache-manifest.c54a2e6486d306d5eadf3b5abcd111ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54989ca3401898af29c94471ec50a22",
+    "url": "/Smart-Brain/index.html"
+  },
+  {
+    "revision": "e298c103953916bd2fd5",
+    "url": "/Smart-Brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f1e30526b4720d1b0b64",
+    "url": "/Smart-Brain/static/css/main.42074c44.chunk.css"
+  },
+  {
+    "revision": "e298c103953916bd2fd5",
+    "url": "/Smart-Brain/static/js/2.1d138cb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Smart-Brain/static/js/2.1d138cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e30526b4720d1b0b64",
+    "url": "/Smart-Brain/static/js/main.102ba638.chunk.js"
+  },
+  {
+    "revision": "c4287044f47a33c0adc6",
+    "url": "/Smart-Brain/static/js/runtime-main.b1a0123d.js"
+  }
+]);
