5d7825a1b4141569ac212d42c8197b74360322da
sidhant <sidhantsharma293@gmail.com>
2020-07-01 16:58:31 +0530

    connected to database

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignin=()=>{
-        fetch('http://localhost:3000/signin',{ 
+        fetch('https://whispering-escarpment-11524.herokuapp.com/signin',{ 
             method:'post',
             headers:{'Content-Type':'application/json'},
             body: JSON.stringify({
